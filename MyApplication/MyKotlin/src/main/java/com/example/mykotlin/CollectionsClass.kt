2520060423511a3659d8da2fc027c4bc40dc2a62
Bitan Atarthy <bitan.atarthy07@gmail.com>
2023-07-19 22:24:42 +0530

    Day 3

---
+++ MyApplication/MyKotlin/src/main/java/com/example/mykotlin/CollectionsClass.kt
@@ -0,0 +1,56 @@
+package com.example.mykotlin
+
+fun main() {
+
+    val myListOfName = listOf("James", "Paul", "Rafael", "Gina")
+//    println(myListOfName)
+
+    // mutable
+    val myMutableList = mutableListOf(12, 34, 45, 123)
+
+//  Unique Value
+    val mySet = setOf("US", "MZ", "AU")
+    val myMutableSet = mutableSetOf(1,2,3,4,5)
+    myMutableSet.add(34)
+    println(myMutableSet)
+
+//    Map
+    val myMap = mapOf("Up" to 1, "Down" to 2)
+    val myMutableMap = mutableMapOf("One" to 1, "Two" to 2)
+    myMutableMap["Three"] = 3
+    println(myMutableMap.values)
+
+    val myNewList = mutableListOf<String>() // empty list
+    myNewList.add("Bitan")
+
+    val empty = emptyList<String>()
+    val emptySet = emptySet<String>()
+    val emptyMap = emptyMap<String, Int>()
+
+
+//    myMutableList.add(214)
+//    myMutableList.remove(214)
+//    myMutableList.removeAt(0)
+//
+//    println("Number of elements ${myMutableList.size}")
+//    println("Second element ${myMutableList[1]}") // .get(index)
+//    println("Index of element \"45\" is ${myMutableList.indexOf(45)}")
+
+
+
+//    println(myMutableList)
+
+//    myListOfName.forEach {
+//        println(it)
+//    }
+
+//    for (item in myListOfName){
+//        println("Name : $item")
+//    }
+}
+
+// List, ArrayList, listOf()
+// Length/size -> no. of elements
+
+
+
--- MyApplication/MyKotlin/src/main/java/com/example/mykotlin/LambdaClass.kt
+++ MyApplication/MyKotlin/src/main/java/com/example/mykotlin/LambdaClass.kt
@@ -1,6 +1,17 @@
 package com.example.mykotlin
 
 fun main() {
-
+    println(sum(12,13))
+    println(calculateCatAge(3))
+    showName("Bitan")
 }
+
+val sum: (Int, Int) -> Int = {
+    a,b -> a + b
+}
+
+val showName : (String) -> Unit = { println(it) }
+
+val calculateCatAge: (Int) -> Int = {it * 7}
+
 
